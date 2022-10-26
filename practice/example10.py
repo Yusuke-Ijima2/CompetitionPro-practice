a880b380e44a9b2e3efcf44815ff79f2d956d127
Yusuke-Ijima2 <yusuke9251120@gmail.com>
2022-10-26 10:27:16 +0900

    change or add 'M  practice/example10.py'

--- practice/example10.py
+++ practice/example10.py
@@ -19,18 +19,21 @@ DY = [0,1,0,-1,1,1,-1-1]
 # 入力
 H,W = map(int,input().split())
 S = [input() for i in range(H)]
+# print(S)
 
 # 答えを表すには二次元配列を用意する('.'のところは0とする)
 result = [[0 if v == '.' else '#' for v in row] for row in S]
+# print(result)
 
 # 各マス(i,j)を順に処理
 for i in range(H):
     for j in range(W):
-        # 飽きます以外はそsのままにする
+        # 空きマス以外はそのままにする
         if S[i][j] != '.':
             continue
         
         # 周囲8マスの'#'の個数を数える
+        # 2つの処理を同時にしている
         for dx ,dy in zip(DX,DY):
             ni,nj = i + dx, j + dy
             
