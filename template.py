1a6a921d48ecb7ca392862111b21bf9123af25d3
Yusuke-Ijima2 <yusuke9251120@gmail.com>
2022-10-27 11:50:03 +0900

    change or add ' M practice/example10.py A  template.py ?? practice/abc081_a.py ?? practice/abc083_b.py ?? practice/abc086_a.py ?? practice/abc088_b.py ?? practice/bc081_b.py ?? practice/bc087_b.py'

---
+++ template.py
@@ -0,0 +1,33 @@
+import io
+import sys
+
+# input here
+_INPUT = """\
+1
+2 2 2
+333
+4 5
+test
+"""
+sys.stdin = io.StringIO(_INPUT)
+
+# output here
+_OUTPUT = """\
+
+"""
+
+# ------------ your code here ---------------
+
+A = int(input())
+# print(A) → 1
+B = list(map(int, input().split()))
+# print(list(B)) → [2, 2, 2]
+C = list(input())
+# print(C) → ['3', '3', '3']
+D,E = list(map(int, input().split()))
+# print(D)→ 4
+# print(E)→ 5
+F = input()
+# print(F)→ test
+
+print(A,B,C,D,E,F)
